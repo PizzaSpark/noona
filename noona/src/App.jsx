e4ccb127eb52962f61ca19a62259d1362473b575
Seanti <seanpizzaspark@gmail.com>
2024-05-22 22:55:18 +0800

    init pages

--- noona/src/App.jsx
+++ noona/src/App.jsx
@@ -7,16 +7,25 @@ import Forbidden from "./pages/Forbidden";
 import ManageMenu from "./pages/ManageMenu";
 import ManageAdmins from "./pages/ManageAdmins";
 import Login from "./pages/Login";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Location from "./pages/Location";
+import Socials from "./pages/Socials";
+
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Landing />} />
-                <Route path="/managemenu" element={<ManageMenu />} />
-                <Route path="/manageadmins" element={<ManageAdmins />} />
+                <Route path="/menu" element={<Menu />} />
+                <Route path="/location" element={<Location />} />
+                <Route path="/socials" element={<Socials />} />
+                <Route path="/about" element={<About />} />
 
                 <Route path="/login" element={<Login />} />
+                <Route path="/managemenu" element={<ManageMenu />} />
+                <Route path="/manageadmins" element={<ManageAdmins />} />
 
                 <Route path="*" element={<NotFound />} />
                 <Route path="/forbidden" element={<Forbidden />} />
